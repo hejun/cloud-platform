b428895bc09c030fc99cb94d876aac9bf2a2d7d8
HeJun <1255524587@qq.com>
2020-12-23 09:59:45 +0800

    引入Common模块

---
+++ common/src/main/java/com/hejun/colud/common/constants/ResultCode.java
@@ -0,0 +1,19 @@
+package com.hejun.colud.common.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 结果状态码枚举
+ *
+ * @author HeJun
+ */
+@Getter
+@AllArgsConstructor
+public enum ResultCode {
+
+    OK(200, "成功"),
+    ;
+    private Integer code;
+    private String desc;
+}
