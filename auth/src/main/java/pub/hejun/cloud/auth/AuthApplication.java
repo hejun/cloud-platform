4d1f6e8f49f160dc60cb0ca2edbe4aee234f3e91
HeJun <1255524587@qq.com>
2021-01-05 23:25:35 +0800

    Auth模块

---
+++ auth/src/main/java/pub/hejun/cloud/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package pub.hejun.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证服务
+ *
+ * @author HeJun
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
