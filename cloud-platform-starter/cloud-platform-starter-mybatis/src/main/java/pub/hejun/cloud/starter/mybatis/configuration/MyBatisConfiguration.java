ee10cfbb42b1b708e6eb9a3f9fbc965c244b7eb6
HeJun <1255524587@qq.com>
2021-04-17 15:41:55 +0800

    修改

--- cloud-platform-starter/cloud-platform-starter-mybatis/src/main/java/pub/hejun/cloud/starter/mybatis/configuration/MyBatisConfiguration.java
+++ cloud-platform-starter/cloud-platform-starter-mybatis/src/main/java/pub/hejun/cloud/starter/mybatis/configuration/MyBatisConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.util.CollectionUtils;
 
@@ -21,7 +20,6 @@ import java.util.List;
  *
  * @author HeJun
  */
-@Configuration
 @ConditionalOnBean(DataSource.class)
 public class MyBatisConfiguration {
 
